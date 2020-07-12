7b8c61fdd57fa2312ba5dbb3cc8480305ea65d5f
James Kessler <afinetooth@gmail.com>
2020-07-12 16:07:52 -0700

    Finish coveralls setup.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,8 @@
 require 'simplecov'
+require 'simplecov-lcov'
+
+SimpleCov::Formatter::LcovFormatter.config.report_with_single_file = true
+SimpleCov.formatter = SimpleCov::Formatter::LcovFormatter
 SimpleCov.start do
   add_filter "/spec/"
 end
